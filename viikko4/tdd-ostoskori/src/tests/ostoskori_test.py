a1dab68cf44b3e0f445c3cfe7b055e9579f5e615
Aarni <aarni.miikkulainen@helsinki.fi>
2022-11-27 20:08:45 +0200

    ostoskorin hinta paivittyy oikein kun lisää kaksi eri tuotetta

--- viikko4/tdd-ostoskori/src/tests/ostoskori_test.py
+++ viikko4/tdd-ostoskori/src/tests/ostoskori_test.py
@@ -26,3 +26,10 @@ class TestOstoskori(unittest.TestCase):
         maito = Tuote("Maito", 3)
         self.kori.lisaa_tuote(maito)
         self.assertEqual(self.kori.hinta(), 3)
+
+    def test_kahden_tuotteen_lisaamisen_jalkeen_korin_hinta_paivittyy(self):
+        maito = Tuote("Maito", 3)
+        olut = Tuote("Olut", 5)
+        self.kori.lisaa_tuote(maito)
+        self.kori.lisaa_tuote(olut)
+        self.assertEqual(self.kori.hinta(), 8)
